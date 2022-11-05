8c12b34d5b0c338ccc578a8fc378b26f77f4d788
adurga-ariqt <adurga@ariqt.com>
2022-11-05 16:16:10 +0530

    Add project files.

---
+++ Test1_Bhavani_SimpleInterest/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test1_Bhavani_SimpleInterest;
+
+Console.WriteLine("Hello, World!");
+SimpleInterest objSimpleInterest = new SimpleInterest();
+objSimpleInterest.SimpleInterestMethod();
---
+++ Test1_Bhavani_SimpleInterest/SimpleInterest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1_Bhavani_SimpleInterest
+{
+    public  class SimpleInterest
+    {
+        public void SimpleInterestMethod()
+        {
+            double p, r, t, si;
+            Console.WriteLine("Enter principal amout");
+            p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter rate of interest");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter time:");
+            t = Convert.ToDouble(Console.ReadLine());
+            double T = t / 12;
+            Console.WriteLine(T);
+            si = p * r * (T) / 100;
+            Console.WriteLine("Simpleinterest is:" + si);
+            double z = si + p;
+            Console.WriteLine("the total amount is:" + z);
+            Console.ReadLine();
+
+        }
+    }
+}
